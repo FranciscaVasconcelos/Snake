50c49783c83f0e3a386dd7100a6fe7147828131f
FranciscaVasconcelos <frannycisca@gmail.com>
2017-01-28 19:49:37 -0500

    Square+Snake classes

--- test.cpp
+++ test.cpp
@@ -12,14 +12,82 @@
 	#  include <GL/glut.h>
 	#endif
 
+int direction = 0; // 0=UP, 1=RIGHT, 2=DOWN, 3=LEFT
+
+class Square {
+	private:
+    	double x, y;
+    	int size;
+  	public:
+  		Square(double,double,int);
+    	void set_values(double,double,int);
+    	void draw();
+
+    Square(double x1, double y1, int size1){
+		x = x1;
+		y = y1;
+		size = size1;
+	}
+
+	// set functions
+	void set_X(double x1) {x = x1;}
+	void set_Y(double y1) {y = y1;}
+	void set_Size(int s1) {size = s1;}
+
+	// get functions
+	double get_X() {return x;}
+	double set_Y() {return y;}
+	int set_Size() {return size;}
+
+	void draw(){
+		glVertex3f(x, y, 0.0); 
+		glVertex3f(x, y+size, 0.0); 
+		glVertex3f(x+size, y+size, 0.0); 
+		glVertex3f(x+size, y, 0.0);
+	}
+};
+
+class Snake {
+	private:
+    	int direction;
+    	int length;
+    	int unit_length;
+    	double grid[][];
+  	public:
+  		Snake(int, int);
+    	void set_values(int);
+    	void draw();
+
+    Snake(int d, int ul, int l){
+		direction=d
+		unit_length=ul
+		length=l
+	}
+
+	// set functions
+	void set_Direction(int d) {direction = d;}
+	void set_UnitLength(int ul) {unit_length = ul;}
+	void set_Length(int l) {length = l;}
+
+	// get functions
+	int get_Direction() {return direction;}
+	int set_UnitLength() {return unit_length;}
+	int set_Length() {return length;}
+
+	void draw(){
+		glVertex3f(x, y, 0.0); 
+		glVertex3f(x, y+size, 0.0); 
+		glVertex3f(x+size, y+size, 0.0); 
+		glVertex3f(x+size, y, 0.0);
+	}
+};
+
 void display(void) { 
 	glClear( GL_COLOR_BUFFER_BIT); 
 	glColor3f(0.0, 1.0, 0.0); 
 	glBegin(GL_POLYGON); 
-	glVertex3f(20.0, 40.0, 0.0); 
-	glVertex3f(80.0, 40.0, 0.0); 
-	glVertex3f(80.0, 60.0, 0.0); 
-	glVertex3f(20.0, 60.0, 0.0); 
+	Square* r = new Square(0.0,0.0,10);
+	r->draw();
 	glEnd(); 
 	glFlush(); 
 }
@@ -27,11 +95,24 @@ void display(void) {
 void keySpecial(int key, int x, int y) {
 	switch(key) {
 		case 27 :      break;
-		case 100 : printf("GLUT_KEY_LEFT %d\n",key);   break;
-		case 102: printf("GLUT_KEY_RIGHT %d\n",key);  ;  break;
-		case 101   : printf("GLUT_KEY_UP %d\n",key);  ;  break;
-		case 103 : printf("GLUT_KEY_DOWN %d\n",key);  ;  break;
+		case 100 : 
+			printf("GLUT_KEY_LEFT %d\n",key);
+			direction=3;  
+			break;
+		case 102: 
+			printf("GLUT_KEY_RIGHT %d\n",key);
+			direction=1;  
+			break;
+		case 101: 
+			printf("GLUT_KEY_UP %d\n",key);
+			direction=0;
+			break;
+		case 103: 
+			printf("GLUT_KEY_DOWN %d\n",key);
+			direction=2;
+			break;
 	};
+	glutPostRedisplay(); 
 }
 
 int main(int argc, char **argv) {
