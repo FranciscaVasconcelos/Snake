53c530252a459849e47670f9e234e676f7463a5b
FranciscaVasconcelos <frannycisca@gmail.com>
2017-01-29 16:01:24 -0500

    General game framework established.

--- test.cpp
+++ test.cpp
@@ -12,107 +12,107 @@
 	#  include <GL/glut.h>
 	#endif
 
-int direction = 0; // 0=UP, 1=RIGHT, 2=DOWN, 3=LEFT
+#define GAME_STATE 		1	// 0=splash page, 1=play game
+#define GAME_SIZE 		50	// # of squares in game
+#define SQUARE_UNIT 	10	// size of sqaure in terms of pixels
+
+/***************************************************************/
+/*************************** CLASSES ***************************/
+/***************************************************************/
 
 class Square {
 	private:
     	double x, y;
     	int size;
   	public:
-  		Square(double,double,int);
-    	void set_values(double,double,int);
-    	void draw();
+	    // constructor
+	    Square(double x1, double y1, int size1){
+			x = x1;
+			y = y1;
+			size = size1;
+		}
 
-    Square(double x1, double y1, int size1){
-		x = x1;
-		y = y1;
-		size = size1;
-	}
+		// set functions
+		void set_X(double x1) {x = x1;}
+		void set_Y(double y1) {y = y1;}
+		void set_Size(int s1) {size = s1;}
 
-	// set functions
-	void set_X(double x1) {x = x1;}
-	void set_Y(double y1) {y = y1;}
-	void set_Size(int s1) {size = s1;}
+		// get functions
+		double get_X() {return x;}
+		double set_Y() {return y;}
+		int set_Size() {return size;}
 
-	// get functions
-	double get_X() {return x;}
-	double set_Y() {return y;}
-	int set_Size() {return size;}
-
-	void draw(){
-		glVertex3f(x, y, 0.0); 
-		glVertex3f(x, y+size, 0.0); 
-		glVertex3f(x+size, y+size, 0.0); 
-		glVertex3f(x+size, y, 0.0);
-	}
+		void draw(){
+			glVertex3f(x, y, 0.0); 
+			glVertex3f(x, y+size, 0.0); 
+			glVertex3f(x+size, y+size, 0.0); 
+			glVertex3f(x+size, y, 0.0);
+			glutPostRedisplay();
+		}
 };
 
-class Snake {
+class Snake : public Square{
 	private:
-    	int direction;
-    	int length;
-    	int unit_length;
-    	double grid[][];
+    	int number; // number in snake queue, when block is at tail, value is 0
   	public:
-  		Snake(int, int);
-    	void set_values(int);
-    	void draw();
+	  	// constructor
+	    Snake(double x1, double y1, int s1, int num):Square(x1,y1,s1){
+			number = num;
+		}
 
-    Snake(int d, int ul, int l){
-		direction=d
-		unit_length=ul
-		length=l
-	}
+		// set functions
+		void set_Number(int n) {number = n;}
 
-	// set functions
-	void set_Direction(int d) {direction = d;}
-	void set_UnitLength(int ul) {unit_length = ul;}
-	void set_Length(int l) {length = l;}
+		// get functions
+		int get_Number() {return number;}
 
-	// get functions
-	int get_Direction() {return direction;}
-	int set_UnitLength() {return unit_length;}
-	int set_Length() {return length;}
-
-	void draw(){
-		glVertex3f(x, y, 0.0); 
-		glVertex3f(x, y+size, 0.0); 
-		glVertex3f(x+size, y+size, 0.0); 
-		glVertex3f(x+size, y, 0.0);
-	}
 };
+
+class Comida : public Square{
+  	public:
+  		// constructor
+	    Comida(double x1, double y1, int s1):Square(x1,y1,s1){
+		}
+};
+
+
+/***************************************************************/
+/************************** GAME CODE **************************/
+/***************************************************************/
+
+
+
+Square *GAME_GRID[GAME_SIZE][GAME_SIZE] = {NULL};
 
 void display(void) { 
 	glClear( GL_COLOR_BUFFER_BIT); 
 	glColor3f(0.0, 1.0, 0.0); 
-	glBegin(GL_POLYGON); 
-	Square* r = new Square(0.0,0.0,10);
-	r->draw();
+	switch(GAME_STATE){
+		case 0: // SPLASH
+			break;
+		case 1: // PlAY GAME
+			break;
+	} 
+	glutPostRedisplay();
 	glEnd(); 
-	glFlush(); 
+	glFlush();
 }
 
 void keySpecial(int key, int x, int y) {
 	switch(key) {
-		case 27 :      break;
 		case 100 : 
 			printf("GLUT_KEY_LEFT %d\n",key);
-			direction=3;  
 			break;
 		case 102: 
 			printf("GLUT_KEY_RIGHT %d\n",key);
-			direction=1;  
 			break;
 		case 101: 
 			printf("GLUT_KEY_UP %d\n",key);
-			direction=0;
 			break;
 		case 103: 
 			printf("GLUT_KEY_DOWN %d\n",key);
-			direction=2;
 			break;
 	};
-	glutPostRedisplay(); 
 }
 
 int main(int argc, char **argv) {
@@ -122,7 +122,7 @@ int main(int argc, char **argv) {
 
 	// initialize window
 	glutInitWindowPosition(100,100); 
-	glutInitWindowSize(500,500); 
+	glutInitWindowSize(GAME_SIZE*SQUARE_UNIT,GAME_SIZE*SQUARE_UNIT); 
 	glutCreateWindow ("SNAKE 2000");
 
 	glutSpecialFunc(keySpecial);
@@ -131,6 +131,7 @@ int main(int argc, char **argv) {
 	glMatrixMode(GL_PROJECTION);              // setup viewing projection 
 	glLoadIdentity();                           // start with identity matrix 
 	glOrtho(0.0, 500.0, 0.0, 500.0, 0.0, 500.0);   // setup a 10x10x2 viewing world
+	 
 
 	glutDisplayFunc(display); 
 	glutMainLoop();
